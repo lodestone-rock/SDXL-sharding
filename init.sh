5e13daf8afbe6df156ab10862b418924313db55f
lodestone-rock <lodestone.rock@gmail.com>
2023-08-20 20:03:54 +0000

    add tensorboard profiler to the virtual env

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 virtualenv -p python3.9 test_env
 test_env/bin/python3 -m pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 test_env/bin/python3 -m pip install transformers diffusers flax optax
+test_env/bin/python3 -m pip install tf-nightly tb-nightly tbp-nightly
